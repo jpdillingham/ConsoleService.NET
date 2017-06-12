73b722d3eb07055cfd4b5f8959fbbf5f58f33d46
JP Dillingham <jpdillingham@gmail.com>
2017-06-11 21:06:59 -0500

    Added service, service installer and basic program.cs modifications.

--- ConsoleService.NET/Program.cs
+++ ConsoleService.NET/Program.cs
@@ -6,10 +6,73 @@ using System.Threading.Tasks;
 
 namespace ConsoleService.NET
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        #region Internal Methods
+
+        internal static void Start(string[] args)
         {
+            Console.WriteLine("Started!");
         }
+
+        internal static void Stop()
+        {
+            Console.WriteLine("Stopped.");
+        }
+
+        #endregion Internal Methods
+
+        #region Private Methods
+
+        /// <summary>
+        ///     Installs or uninstalls the application as a Windows Service, depending on the provided action.
+        /// </summary>
+        /// <param name="action">The action to perform (uninstall or install).</param>
+        /// <returns>True if the installation/uninstallation succeeded, false otherwise.</returns>
+        [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+        public static bool ModifyService(string action)
+        {
+            try
+            {
+                if (action == "uninstall")
+                {
+                    System.Configuration.Install.ManagedInstallerClass.InstallHelper(new string[] { "/u", System.Reflection.Assembly.GetExecutingAssembly().Location });
+                }
+                else
+                {
+                    System.Configuration.Install.ManagedInstallerClass.InstallHelper(new string[] { System.Reflection.Assembly.GetExecutingAssembly().Location });
+                }
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                if (args[0] == "install")
+                {
+                    ModifyService("install");
+                }
+                else if (args[0] == "uninstall")
+                {
+                    ModifyService("uninstall");
+                }
+            }
+
+            Start(args);
+
+            Console.WriteLine("Press any key to stop...");
+            Console.ReadKey();
+
+            Stop();
+        }
+
+        #endregion Private Methods
     }
 }
---
+++ ConsoleService.NET/Service.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleService.NET
+{
+    partial class ConsoleService : ServiceBase
+    {
+        #region Public Constructors
+
+        public ConsoleService()
+        {
+            InitializeComponent();
+        }
+
+        #endregion Public Constructors
+
+        #region Protected Methods
+
+        protected override void OnStart(string[] args)
+        {
+            // set the working directory for the application to the location of the executable. if this is not set here, the
+            // application believes it is running from %windir%\system32\.
+            System.IO.Directory.SetCurrentDirectory(System.AppDomain.CurrentDomain.BaseDirectory);
+            Program.Start(args);
+        }
+
+        protected override void OnStop()
+        {
+            Program.Stop();
+        }
+
+        #endregion Protected Methods
+    }
+}

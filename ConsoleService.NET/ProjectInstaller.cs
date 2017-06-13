8c7747678f87cd5fd80cfb96861b0fcc468ca8d2
JP Dillingham <jpdillingham@gmail.com>
2017-06-13 18:27:45 -0500

    ProjectInstaller cleanup

--- ConsoleService.NET/ProjectInstaller.cs
+++ ConsoleService.NET/ProjectInstaller.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Configuration.Install;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 
 namespace ConsoleService.NET
 {
+    /// <summary>
+    ///     The installer for the Service.
+    /// </summary>
     [RunInstaller(true)]
     public partial class ProjectInstaller : System.Configuration.Install.Installer
     {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="ProjectInstaller"/> class.
+        /// </summary>
         public ProjectInstaller()
         {
             InitializeComponent();
